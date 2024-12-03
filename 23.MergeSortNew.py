0330cee5db92b2ee13d9d9d500b52525b6b91fbf
Ahmad Ashril Rizal <ashril.rizal@gmail.com>
2024-12-03 13:09:55 +0800

    Update 23.MergeSortNew.py

--- 23.MergeSortNew.py
+++ 23.MergeSortNew.py
@@ -1,23 +1,3 @@
-#Input Bilangan dalam List
-#data = [5,4,3,1,9,8,10]
-# ok = "y" 
-# while (ok=="y"):
-# 	data.append(input("Masukkan Bilangan: "))
-# 	ok = raw_input("Lanjut? (y/n) : ")
-# print "Bilangan dalam array adalah: ",data
-
-#Insertion Sort
-#print data
-#kiri = 1
-#kanan = n
-#i = kiri
-#x = 1
-#if (n>1):
-#	while (i<kanan):
-#		if (issetor )
-
-#print "Bilangan setelah diurutkan dengan Insertion Sort adalah: ",data
-
 # Data awal
 data = [6, 5, 3, 1, 8, 7, 2, 4]
 print("Array sebelum diurutkan:", data)
